58ad9ab535dbf3a192f9bf25ee26452a7a080344
Mateus-de-Moura <mateus.demoura@hotmail.com>
2022-10-03 22:36:59 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("SmartPhone Nokia");
+Smartphone nokia = new Nokia(numero: "1111111", modelo: "Modelo 1", IMEI: "222222", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("SmartPhone Iphone");
+Smartphone Iphone = new Iphone(numero: "1111111", modelo: "Modelo 1", IMEI: "222222", 64);
+Iphone.Ligar();
+Iphone.InstalarAplicativo("Instagram");
